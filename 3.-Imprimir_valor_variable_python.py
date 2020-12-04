43644cb8ec735e4ea2e1798cbebba54a1b455d26
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:34:01 -0600

    Add files via upload

---
+++ 3.-Imprimir_valor_variable_python.py
@@ -0,0 +1,5 @@
+# Este programa muestra como imprimir el valor de una variable.
+numero = 7
+print("El valor de la variable es: ")
+print(numero)
+# Este programa fue escrito por Emilio Carcaño Bringas.
